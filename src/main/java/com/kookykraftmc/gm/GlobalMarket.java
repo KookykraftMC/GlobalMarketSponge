3c2c1ea4d7ca21adaa09c0bc9e40119a74e12848
SolarShrieking <michael.anible@gmail.com>
2016-12-01 16:32:35 -0500

    Burned Time's code alive

--- src/main/java/com/kookykraftmc/gm/GlobalMarket.java
+++ src/main/java/com/kookykraftmc/gm/GlobalMarket.java
@@ -25,66 +25,11 @@ public class GlobalMarket {
     @Inject
     private Logger logger;
 
-    @Inject
-    @ConfigDir(sharedRoot = false)
-    private File cfgDir;
-
-    @Inject
-    @ConfigDir(sharedRoot = false)
-    private File defaultCfg;
-
-    @Inject
-    @ConfigDir(sharedRoot = false)
-    private ConfigurationLoader<CommentedConfigurationNode> loader;
-
-    @Inject
-    @ConfigDir(sharedRoot = false)
-    private File dataCfgDir;
-
-    @Inject
-    @ConfigDir(sharedRoot = false)
-    private File dataDefaultCfg;
-
-    private ConfigurationNode cfg;
-    private ConfigurationNode cfgData;
-
     Game game = Sponge.getGame();
 
     @Listener
     public void preInit(GamePreInitializationEvent event) {
-        makeConfig();
+
     }
 
-    public void addItemToData(ItemStack stack, Integer price) {
-        try {
-            cfgData = loader.load();
-            int id = cfgData.getNode("data", "last-id").getInt();
-            if (id == 0) {
-                cfg.getNode("data", "last-id").setValue(1);
-            }
-            if (!dataDefaultCfg.exists()) {
-                cfgData.getNode("data").setValue(18);
-                loader.save(cfgData);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    private void makeConfig() {
-        try {
-            cfg = loader.load();
-            if (!defaultCfg.exists()) {
-                cfg.getNode("stack-slots").setValue(18);
-                cfg.getNode("stack-slots-max").setValue("320");
-                cfg.getNode("tax").setValue(0.05);
-                cfg.getNode("command-name").setValue("market");
-                cfg.getNode("enable-server-listings").setValue("true");
-                loader.save(cfg);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
